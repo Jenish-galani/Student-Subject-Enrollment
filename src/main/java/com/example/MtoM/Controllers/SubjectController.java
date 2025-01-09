3fa5a669dd512a5b7182bab593a2c9625d7a4872
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-09 11:38:54 +0530

    Update SubjectController.java
    .

--- src/main/java/com/example/MtoM/Controllers/SubjectController.java
+++ src/main/java/com/example/MtoM/Controllers/SubjectController.java
@@ -8,24 +8,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("subject")
+@RestController // Indicates this class is a REST controller
+@RequestMapping("subject") // Maps HTTP requests with "subject" to this controller
 public class SubjectController {
-    @Autowired
+
+    @Autowired // Injects the SubjectService dependency
     private SubjectService subjectService;
 
+    // Handles HTTP POST requests to save a new Subject
     @PostMapping()
     public Subject insert(@RequestBody Subject subject) {
         return subjectService.insertSubject(subject);
     }
 
+    // Handles HTTP GET requests to retrieve a Subject by its ID
     @GetMapping("id/{sId}")
     public Optional<Subject> showOne(@PathVariable Long sId) {
         return subjectService.showOne(sId);
     }
 
+    // Handles HTTP GET requests to retrieve all Subjects
     @GetMapping()
     public List<Subject> getAll() {
         return subjectService.getAll();
     }
 }
+
