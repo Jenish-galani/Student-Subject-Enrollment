eed136e425081c2744ea2b126dfb7f48461dd7bb
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-09 11:39:37 +0530

    Update StudentRepo.java
    .

--- src/main/java/com/example/MtoM/Repositories/StudentRepo.java
+++ src/main/java/com/example/MtoM/Repositories/StudentRepo.java
@@ -5,6 +5,6 @@ import com.example.MtoM.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // This interface is a Spring Data JPA repository for the Student entity
 public interface StudentRepo extends JpaRepository<Student, Long> {
 }
