0122dee057e1d37ab63ee6573bad0fd8a86f67f1
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-09 11:37:52 +0530

    Update SubjectService.java
    .

--- src/main/java/com/example/MtoM/Services/SubjectService.java
+++ src/main/java/com/example/MtoM/Services/SubjectService.java
@@ -8,19 +8,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service // Marks this class as a Spring service component
 public class SubjectService {
-    @Autowired
+    
+    @Autowired // Injects the SubjectRepo dependency
     private SubjectRepo subjectRepo;
 
+    // Saves a new Subject to the database
     public Subject insertSubject(Subject subject) {
         return subjectRepo.save(subject);
     }
 
+    // Retrieves a specific Subject by its ID
     public Optional<Subject> showOne(Long sId) {
         return subjectRepo.findById(sId);
     }
 
+    // Retrieves a list of all Subjects from the database
     public List<Subject> getAll() {
         return subjectRepo.findAll();
     }
