d4c7c17881a1b704e422829d8463f8a7f2190aa7
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-09 11:40:18 +0530

    Update SubjectRepo.java
    .

--- src/main/java/com/example/MtoM/Repositories/SubjectRepo.java
+++ src/main/java/com/example/MtoM/Repositories/SubjectRepo.java
@@ -4,6 +4,6 @@ import com.example.MtoM.Models.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // This interface is a Spring Data JPA repository for the Subject entity
 public interface SubjectRepo extends JpaRepository<Subject, Long> {
 }
